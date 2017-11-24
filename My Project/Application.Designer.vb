d8b648fbecf23e90c840af20cecc043000d1a768
Yuki Moriyama <y_moriyama_1996@hotmail.co.jp>
2017-11-24 10:11:28 +0900

    ログイン画面にメインフォームを変更。

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.NurseryTeacher.Form5
+            Me.MainForm = Global.NurseryTeacher.Login
         End Sub
     End Class
 End Namespace
